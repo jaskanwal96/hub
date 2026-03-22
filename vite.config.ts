6417edd6e7c70952200ed51c90713ae0c9e15914
Jaskanwal Singh <jaskanwal196@gmail.com>
2026-03-22 20:18:40 +0900

    Panning init

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [
@@ -24,4 +25,7 @@ export default defineConfig({
     }),
   ],
   base: "./",
+  resolve: {
+    alias: { "@": resolve(__dirname, "src") },
+  },
 });
