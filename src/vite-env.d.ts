acdc12a3c5cc3331a84213ffc9e02681903ccb23
Birinder Singh <birindersingh2410@gmail.com>
2026-05-15 14:07:55 +0900

    Upgrading Node

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -9,5 +9,6 @@ interface Window {
   storage: {
     get(key: string): Promise<{ value: string } | null>;
     set(key: string, value: string): Promise<void>;
+    delete(key: string): Promise<void>;
   };
 }
